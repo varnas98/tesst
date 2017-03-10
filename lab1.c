dec73b2d1f6e671a0c459c87fc3384b7c562313b
varnas98 <varnas98@mail.ru>
2017-03-10 09:41:19 +0200

    2

--- lab1.c
+++ lab1.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+
+int EditMoney(){
+	int money; //Размер вклада
+	do{
+		printf("Введите размер вклада (в рублях): \n ");
+		scanf("%d", &money);
+		if(money<10000){
+			printf("Сумма вклада должно быть не меннее 10 т.р.!\n");
+		}else break;
+	}while(1);
+	return money;
+}
+
 int main(void){
-	int money;
-	int day ;
+	int money = EditMoney();
+	int day = EditDay();
 	int proc;
 	int itog;
-	
+	if(day<31){
+		itog = money - Procent(money, 10);
+	}else{
+		int srok1, srok2, srok3;
+		if(money<100000){
+			srok1=2; srok2=6; srok3=12; 
+		}else{ 
+			srok1=3; srok2=8; srok3=15; 
+		}
+		itog=money+Vkad(money,day,srok1,srok2,srok3);
+	}
+	printf("Сумму вклада на момент окончания срока: %dр\n", itog);
 	return 0;
 }
